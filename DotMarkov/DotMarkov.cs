edf46d41a00fb7d677ddda04aebbd3020f423344
Oliver Andreas Thunæs <Felolis30@gmail.com>
2024-02-10 16:59:51 +0100

    Half-implemented markov chains

--- DotMarkov/DotMarkov.cs
+++ DotMarkov/DotMarkov.cs
@@ -1,138 +1,245 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace DotMarkov;
 
-public class MarkovGenerator
+public class MarkovGenerator(int level = 2)
 {
-    //                <tokens, possible outcomes>
-    private Dictionary<string, List<string>> corpus;
+    private readonly int level = level;
 
-    public MarkovGenerator(string path)
+    private readonly List<string> sourcePhrases = [];
+
+    public MarkovChain Chain = new();
+
+    public void Learn(List<string> phrases)
     {
-        corpus = [];
-        Tokenize(path);
-
-        string text = GenerateText(100);
-        Console.WriteLine(text);
+        var newTerms = phrases.Where(s => !sourcePhrases.Contains(s));
+        foreach (var phrase in newTerms)
+        {
+            Learn(phrase);
+        }
     }
 
-    private string GenerateText(int length)
+    public void Learn(string phrase)
     {
-        StringBuilder builder = new();
+        if (string.IsNullOrEmpty(phrase))
+            return;
 
-        var rng = new Random();
+        if (SplitTokens(phrase).Count < level)
+            return;
 
-        string prevWord = corpus.Keys.ElementAt(rng.Next(corpus.Keys.Count));
-        builder.Append(prevWord + " ");
+        sourcePhrases.Add(phrase);
 
-        for (int i = 1; i < length; i++)
+        var tokens = SplitTokens(phrase);
+
+        LearnTokens(tokens);
+    }
+
+    private void LearnTokens(List<string> tokens)
+    {
+        for (var i = 0; i < tokens.Count; i++)
         {
-            var possible = corpus[prevWord];
-            if (possible.Count == 0)
-            {
-                prevWord = corpus.Keys.ElementAt(rng.Next(corpus.Keys.Count));
-                builder.Append(prevWord + " ");
-                continue;
-            }
-            int index = rng.Next(possible.Count);
-            var word = possible[index];
-            prevWord = word;
+            var current = tokens[i];
+            var key = new List<string>();
 
-            builder.Append(word + " ");
+            for (var j = level; j > 0; j--)
+            {
+                string previous;
+                try
+                {
+                    if (i - j < 0)
+                    {
+                        key.Add("");
+                    }
+                    else
+                    {
+                        previous = tokens[i - j];
+                        key.Add(previous);
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    previous = "";
+                    key.Add(previous);
+                }
+            }
+
+            // add the current token to the markov model at the composite key
+            Chain.AddOrCreate(key, current);
+        }
+    }
+
+    public List<string> Walk(int lines = 1, string seed = "")
+    {
+        if (lines < 1)
+        {
+            throw new ArgumentException("Invalid argument - line count for walk must be a positive integer", nameof(lines));
         }
 
-        return builder.ToString();
+        var sentences = new List<string>();
+
+        int genCount = 0;
+        int created = 0;
+        while (created < lines)
+        {
+            if (genCount == lines * 10)
+            {
+                Console.WriteLine($"Breaking out of walk early - {genCount} generations did not produce {lines} distinct lines ({sentences.Count} were created)");
+                break;
+            }
+            var result = WalkLine(seed);
+            if ((!EnsureUniqueWalk || !SourcePhrases.Contains(result)) && (!EnsureUniqueWalk || !sentences.Contains(result)))
+            {
+                sentences.Add(result);
+                created++;
+                yield return result;
+            }
+            genCount++;
+        }
     }
 
-    private void Tokenize(string path)
+    private string WalkLine(string seed)
     {
-        // Length in words of the token key
-        foreach (var line in File.ReadLines(path))
+        var paddedSeed = PadArrayLow(SplitTokens(seed)?.ToArray());
+        var built = new List<string>();
+
+        // Allocate a queue to act as the memory, which is n 
+        // levels deep of previous words that were used
+        var q = new Queue(paddedSeed);
+
+        // If the start of the generated text has been seeded,
+        // append that before generating the rest
+        if (!seed.Equals(GetPrepadUnigram()))
         {
-            if (line == "") continue;
+            built.AddRange(SplitTokens(seed));
+        }
 
-            var words = line.Split(' ').ToList().FindAll(t => !Regex.IsMatch(t, @"(\d)"));
-
-            for (int i = 0; i < words.Count; i++)
+        while (built.Count < 1500)
+        {
+            // Choose a new token to add from the model
+            var key = new List<string>(q.Cast<string>().ToArray());
+            if (Chain.Contains(key))
             {
-                var token = words[i];
+                string chosen;
 
-                if (!corpus.TryGetValue(token, out List<string>? value))
+                if (built.Count == 0)
                 {
-                    value = [];
-                    corpus.Add(token, value);
+                    chosen = new UnweightedRandomUnigramSelector<TUnigram>().SelectUnigram(Chain.GetValuesForKey(key));
+                }
+                else
+                {
+                    chosen = UnigramSelector.SelectUnigram(Chain.GetValuesForKey(key));
                 }
 
-                if (i + 1 >= words.Count)
-                    break;
-
-                var nextWord = words[i + 1];
-                value.Add(nextWord);
+                q.Dequeue();
+                q.Enqueue(chosen);
+                built.Add(chosen);
+            }
+            else
+            {
+                break;
             }
         }
 
-        // Debugging stuff
-        // int maxCount = 100;
-        // int iter = 0;
-        // foreach (var (token, outcomes) in corpus)
-        // {
-        //     StringBuilder builder = new();
-        //     builder.Append($"|{token}".PadRight(12));
-
-        //     for (int i = 0; i < outcomes.Count; i++)
-        //     {
-        //         if (i == 0)
-        //             builder.Append("|");
-
-        //         var outcome = outcomes[i];
-        //         builder.Append($"{outcome} -> ");
-
-        //         if (i == outcomes.Count - 1)
-        //             builder.Append("|");
-        //     }
-
-        //     Console.WriteLine(builder);
-
-        //     iter++;
-        //     if (iter >= maxCount)
-        //         break;
-        // }
+        return RebuildPhrase(built);
     }
 
-    // private void GenerateTokens(string path)
+    private static string GetPrepadUnigram()
+    {
+        return " ";
+    }
+
+    private string[] PadArrayLow(string[] input)
+    {
+        if (input == null)
+        {
+            input = new List<string>().ToArray();
+        }
+
+        var splitCount = input.Length;
+        if (splitCount > level)
+        {
+            input = input.Skip(splitCount - level).Take(level).ToArray();
+        }
+
+        var p = new string[level];
+        var j = 0;
+        for (var i = level - input.Length; i < level; i++)
+        {
+            p[i] = input[j];
+            j++;
+        }
+        for (var i = level - input.Length; i > 0; i--)
+        {
+            p[i - 1] = "";
+        }
+
+        return p;
+    }
+
+    private List<string> SplitTokens(string phrase)
+    {
+        return [.. phrase.Split(' ')];
+    }
+
+    private string RebuildPhrase(List<string> tokens)
+    {
+        return string.Join(' ', tokens);
+    }
+}
+
+public class MarkovChain
+{
+    public MarkovChain()
+    {
+        ChainDictionary = new ConcurrentDictionary<List<string>, List<string>>();
+    }
+
+    internal ConcurrentDictionary<List<string>, List<string>> ChainDictionary { get; }
+    private readonly object lockObj = new object();
+
+    /// <summary>
+    /// The number of states in the chain
+    /// </summary>
+    public int Count => ChainDictionary.Count;
+
+    internal bool Contains(List<string> key)
+    {
+        return ChainDictionary.ContainsKey(key);
+    }
+
+    /// <summary>
+    /// Add a TGram to the markov models store with a composite key of the previous [Level] number of TGrams
+    /// </summary>
+    /// <param name="key">The composite key under which to add the TGram value</param>
+    /// <param name="value">The value to add to the store</param>
+    internal void AddOrCreate(List<string> key, string value)
+    {
+        lock (lockObj)
+        {
+            if (!ChainDictionary.ContainsKey(key))
+            {
+                ChainDictionary.TryAdd(key, [value]);
+            }
+            else
+            {
+                ChainDictionary[key].Add(value);
+            }
+        }
+    }
+
+    internal List<string> GetValuesForKey(List<string> key)
+    {
+        return ChainDictionary[key];
+    }
+
+    // internal IEnumerable<StateStatistic<string>> GetStatistics()
     // {
-    //     string lines = "";
-    //     foreach (var line in File.ReadAllLines(path))
-    //     {
-    //         if (string.IsNullOrEmpty(line)) continue;
-    //         lines += line + " ";
-    //     }
+    //     var stats = ChainDictionary.Keys.Select(a => new StateStatistic<string>(a, ChainDictionary[a]))
+    //         .OrderByDescending(a => a.Next.Sum(x => x.Count));
 
-    //     var punctuation = "\\[\\](){}!?.,:;'\"\\/*&^%$_+-–—=<>@|~";
-    //     var ellipsis = "\\.{3}";
-
-    //     var words = "[a-zA-Zа-яА-ЯёЁ]+";
-    //     var compounds = "${words}-${words}";
-
-
-    //     Console.WriteLine($"{punctuation}");
-
-    //     var tokenizeRegex = new Regex($"({ellipsis}|{compounds}|{words}|[{punctuation}])");
-
-    //     var newLineRegex = new Regex("/\ns*/g");
-    //     var tokens = tokenizeRegex.Split(newLineRegex.Replace(lines, "§"))
-    //         .ToList()
-    //         .FindAll((t) =>
-    //             !string.IsNullOrEmpty(t) &&
-    //             !string.IsNullOrWhiteSpace(t) &&
-    //             !Regex.IsMatch(t, "[0-9]|\\:"))
-    //         .ToArray();
-
-    //     foreach (var token in tokens[0..20])
-    //     {
-    //         Console.WriteLine(token);
-    //     }
+    //     return stats;
     // }
-
 }
